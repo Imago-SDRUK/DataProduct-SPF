c73fd0aa128c55d48550ecc7478493f2d8e3249a
BVSh <behzad.valipour-shokouhi@newcastle.ac.uk>
2025-11-13 14:20:06 +0000

    [Dev] - resolved the comments

--- src/dask_code.py
+++ src/dask_code.py
@@ -3,7 +3,7 @@ from dask.distributed import LocalCluster
 from dask import delayed, compute
 from imago_utils.io.downloader import load_tile, to_geotif
 from imago_utils.utils.mask import apply_scl_mask
-import xarray as xr
+from imago_utils.utils.process import process_cloud
 import geopandas as gpd
 import yaml
 
@@ -18,7 +18,6 @@ dask_cfg = config["dask"]
 # for each section, go through and get the variables of interest
 # input params
 tiles_file= input_cfg["tiles"]
-api_url = input_cfg["api_url"]
 collection = input_cfg["collection"]
 start_date = input_cfg["time"]["start_date"]
 end_date = input_cfg["time"]["end_date"]
@@ -26,52 +25,20 @@ out_bands = input_cfg["bands_of_interest"]
 input_storage = input_cfg["input_storage"]
 # output params
 output_storage = output_cfg["output_storage"]
-output_filename = output_cfg["output_filename"]
 target_crs = output_cfg["target_crs"]
 out_resolution = output_cfg["res"]
 target_dtype = output_cfg["target_dtype"]
 resampling = output_cfg["resampling"]
 out_format = output_cfg["out_format"]
-area_name = output_cfg["area_name"]
-# dask/optimisation params
+# dask/optimisation paramsprocess.py
 chunks = dask_cfg["chunks"]
 n_workers = dask_cfg["n_workers"]
 threads_per_worker = dask_cfg["threads_per_worker"]
 memory_limit = dask_cfg["memory_limit"]
 dashboard_address = dask_cfg["dashboard_address"]
 
-# TODO: Find a sutable function name so that it can be realted to the SPF project
-# TODO: Move the function to imago-utils in to process.py
-def process_cloud(cloud_masked):
-    """This function calculates the median cloud probability, counts valid pixels from the xarray dataset, and then export it to GeoTIFFs.
-    Parameters:
-    dataset (xarray.Dataset): dataset with `cloud` and `scl` variables
-    out_path (str): path to the output GeoTIFFs.
-    Returns:
-    cloud_out (xarray.Dataset): output xarray dataset with calculated median probability and valid pixels
-    """
-    cloud_out = xr.Dataset(
-        {
-            "cloud_prob": (
-                cloud_masked.where(cloud_masked != -1)
-                .groupby("time.year")
-                .mean(dim="time", skipna=True)
-                .astype("float32")
-            ),
-            "valid_count": (
-                cloud_masked.where(cloud_masked != -1)
-                .groupby("time.year")
-                .count(dim="time")
-                .astype("int16")
-                # NOTE: `.count` in xarray automatically calculates without NODATA values
-            ),
-        }
-    )
-
-    return cloud_out
-
 @delayed
-def process_tiles(tile_bbox, idx):
+def process_tiles(tile_bbox, tile_name):
     tile = load_tile(
         tile_bbox=tile_bbox,
         collection=collection,
@@ -85,6 +52,7 @@ def process_tiles(tile_bbox, idx):
 
     masked_tile = apply_scl_mask(tile, origin_band="cloud")
     ds = process_cloud(masked_tile)
+    output_filename = 
     to_geotif(ds, out_path=f"{output_storage}/{output_filename}", out_bands=list(ds.data_vars))
 
 def main():
@@ -100,15 +68,14 @@ def main():
     dask_client = DaskClient(cluster)
     dash_url = cluster.dashboard_link
     scheduler_host = dash_url.split("//")[1].split(":")[0]
-    print("\nto access the Dask dashboard, do:")
+    print("\nto access the Dask dashboard, do:",flush=True)
     print(f"ssh -L {dashboard_address.lstrip(':')}{scheduler_host}{dashboard_address} "
-          f"<hpc_username>@<login_node>")
-    print(f"then open: http://localhost{dashboard_address}\n")
-    # print(dask_client.get_versions(check=True))
+          f"<hpc_username>@<login_node>", flush=True)
+    print(f"then open: http://localhost{dashboard_address}\n",flush=True)
 
     tasks = [
-        process_tiles([row.minx, row.miny, row.maxx, row.maxy], idx)
-        for idx, row in tiles.bounds.iterrows()
+        process_tiles([row.minx, row.miny, row.maxx, row.maxy], tiles.loc[row.name, "tile_name"])
+        for _, row in tiles.bounds.iterrows()
     ]
     results = compute(*tasks)
     return results
