b0630fc9832a35e61cdaee5cab016f35130292f5
BVSh <behzad.valipour-shokouhi@newcastle.ac.uk>
2025-11-15 09:23:15 +0000

    improve the code & config file

--- src/dask_code.py
+++ src/dask_code.py
@@ -6,39 +6,21 @@ from imago_utils.io.downloader import load_tile, to_geotif
 from imago_utils.utils.mask import apply_scl_mask
 from imago_utils.utils.process import process_cloud
 import geopandas as gpd
-import yaml
+import yaml, argparse
 
-with open("/home/u5da/bvsh15.u5da/codebook/config_template.yaml", "r") as f:
-    config = yaml.safe_load(f)
-
-# extract sections
-input_cfg = config["input"]
-output_cfg = config["output"]
-dask_cfg = config["dask"]
-
-# for each section, go through and get the variables of interest
-# input params
-tiles_file= input_cfg["tiles"]
-collection = input_cfg["collection"]
-start_date = input_cfg["time"]["start_date"]
-end_date = input_cfg["time"]["end_date"]
-out_bands = input_cfg["bands_of_interest"]
-input_storage = input_cfg["input_storage"]
-# output params
-output_storage = output_cfg["output_storage"]
-output_filename = output_cfg["output_filename"]
-out_crs = output_cfg["out_crs"]
-out_resolution = output_cfg["res"]
-out_dtype = output_cfg["out_dtype"]
-resampling = output_cfg["resampling"]
-out_format = output_cfg["out_format"]
-time_dim = output_cfg["time_dim"]
-# dask/optimisation paramsprocess.py
-chunks = dask_cfg["chunks"]
-n_workers = dask_cfg["n_workers"]
-threads_per_worker = dask_cfg["threads_per_worker"]
-memory_limit = dask_cfg["memory_limit"]
-dashboard_address = dask_cfg["dashboard_address"]
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="SPF",
+        description="Processing of cloud probability for the SPF data product",
+    )
+    parser.add_argument(
+        "--config_path",
+        "-i",
+        help="Absolute or relative path to a config.yaml file",
+        default="config.yaml",
+        required=False,
+    )
+    return parser.parse_args()
 
 def process_tiles(tile_bbox, tile_name):
     tile = load_tile(
@@ -62,22 +44,58 @@ def process_tiles(tile_bbox, tile_name):
               time_dim = time_dim)
 
 def main():
+    args = parse_args()
+
+    with open(args.config_path, "r") as f:
+        config = yaml.safe_load(f)
+    
+    # extract sections
+    input_cfg = config["input"]
+    output_cfg = config["output"]
+    dask_cfg = config["dask"]
+
+    # make globals accessible to process_tiles
+    global tiles_file, collection, start_date, end_date, out_bands
+    global input_storage, output_storage, output_filename, out_crs
+    global out_resolution, out_dtype, resampling, out_format, time_dim
+    global chunks, npartitions, n_workers, threads_per_worker, memory_limit, dashboard_address
+
+    # for each section, go through and get the variables of interest
+    # input params
+    tiles_file= input_cfg["tiles"]
+    collection = input_cfg["collection"]
+    start_date = input_cfg["time"]["start_date"]
+    end_date = input_cfg["time"]["end_date"]
+    out_bands = input_cfg["bands_of_interest"]
+    input_storage = input_cfg["input_storage"]
+    # output params
+    output_storage = output_cfg["output_storage"]
+    output_filename = output_cfg["output_filename"]
+    out_crs = output_cfg["out_crs"]
+    out_resolution = output_cfg["res"]
+    out_dtype = output_cfg["out_dtype"]
+    resampling = output_cfg["resampling"]
+    out_format = output_cfg["out_format"]
+    time_dim = output_cfg["time_dim"]
+    # dask/optimisation paramsprocess.py
+    chunks = dask_cfg["chunks"]
+    npartitions = dask_cfg["npartitions"]
+    n_workers = dask_cfg["n_workers"]
+    threads_per_worker = dask_cfg["threads_per_worker"]
+    memory_limit = dask_cfg["memory_limit"]
+    dashboard_address = dask_cfg["dashboard_address"]
+
     """Main function that generates the cluster and performs the computation"""
     tiles = gpd.read_file(f"{input_storage}/{tiles_file}")
+
+    #TODO: use dask-jobqueue instead of local cluster
     cluster = LocalCluster(
         n_workers=n_workers,
         threads_per_worker=threads_per_worker,
         memory_limit=memory_limit,
         dashboard_address=dashboard_address,
     )
-
     dask_client = DaskClient(cluster)
-    dash_url = cluster.dashboard_link
-    scheduler_host = dash_url.split("//")[1].split(":")[0]
-    print("\nto access the Dask dashboard, do:",flush=True)
-    print(f"ssh -L {dashboard_address.lstrip(':')}:{scheduler_host}{dashboard_address} "
-          f"<hpc_username>@<login_node>", flush=True)
-    print(f"then open: http://localhost{dashboard_address}\n",flush=True)
 
     tile_list = [
         ([row.minx, row.miny, row.maxx, row.maxy], tiles.loc[row.name, "tile_name"])
@@ -85,7 +103,7 @@ def main():
     ]
 
     
-    bag = db.from_sequence(tile_list, npartitions=16) #Break tiles into 16 groups
+    bag = db.from_sequence(tile_list, npartitions=npartitions)
     bag = bag.map(lambda x: process_tiles(x[0], x[1])) # Map the processing function to each tile
     results = bag.compute()
     return results
